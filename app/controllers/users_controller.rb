6543afd5623e8df7e07ab9f13f6750ac22b3e449
AndroAvi <avijeetpandey87@gmail.com>
2023-02-22 21:22:57 +0530

    tweak: add token to user create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { user: UsersBlueprint.render_as_json(@user) }, status: :created
+      render json: { user: UsersBlueprint.render_as_json(@user), token: @user.generate_auth_token }, status: :created
     else
       render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
     end
