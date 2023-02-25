b8e8cf7e1fe937675b08858db3bd2ffdb90e713d
AndroAvi <avijeetpandey87@gmail.com>
2023-02-25 21:04:22 +0530

    bugfix: make error reports uniform

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,17 +16,17 @@ class ApplicationController < ActionController::API
       decoded = JWT.decode(auth_header.split(' ')[1], ENV['JWT_SECRET'], true, algorithm: 'HS256')
       @current_user = User.find(decoded[0]['id'])
     rescue JWT::ExpiredSignature
-      render json: { error: 'Token expired' }, status: :unauthorized
+      render json: { error: ['Token expired'] }, status: :unauthorized
     rescue ActiveRecord::RecordNotFound, JWT::DecodeError
-      render json: { error: 'Invalid token' }, status: :unauthorized
+      render json: { error: ['Invalid token'] }, status: :unauthorized
     end
   end
 
   def not_found
-    render json: { error: 'Record Not found' }, status: :not_found
+    render json: { error: ['Record Not found'] }, status: :not_found
   end
 
   def bad_body
-    render json: { error: 'Invalid body' }, status: :bad_request
+    render json: { error: ['Invalid body'] }, status: :bad_request
   end
 end
